886e2bb523bc6d6324ecc9d7574376d3eabc94b7
MoreiraH <aemh.23506730062@gmail.com>
2024-09-12 15:10:15 -0500

    commit one

---
+++ queuepratice.py
@@ -0,0 +1,14 @@
+containerfilas=[]
+
+containerfilas.append(12)
+containerfilas.append(12)
+containerfilas.append(12)
+
+for i in containerfilas:
+    print(i)
+
+print("AFTER POP")
+
+ctml=ontainerfilas.pop(0)
+for i in containerfilas
+print(i)
